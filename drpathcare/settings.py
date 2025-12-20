2b8ea3dc36c10892e0e28e0529c59f64d886aa56
Aloukik Omar <aloukikomar@Aloukiks-MacBook-Air.local>
2025-12-20 19:25:53 +0530

    settings updated

--- drpathcare/settings.py
+++ drpathcare/settings.py
@@ -276,3 +276,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
